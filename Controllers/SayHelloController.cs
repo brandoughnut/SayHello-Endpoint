be04c35d820bc9a860d89515a1bf938e55da39e2
brandoughnut <brandonnguyen2867@gmail.com>
2024-01-08 10:37:17 -0800

    SayHello

---
+++ Controllers/SayHelloController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SayHello_Endpoint.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+    public class SayHelloController
+    {
+        
+        public List<string> studentName = new();
+
+        [HttpPost]
+        [Route("/UserName/{name}")]
+        public List<string> UserName(string name)
+        {
+            studentName.Add("Hello, "+name);
+            return studentName;
+        }
+
+    }
